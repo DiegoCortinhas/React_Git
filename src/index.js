c390eb3a52474b0c5bdb68765dd8e3bea6498551
Diego Pereira Cortinhas <diego.cortinhas@gmail.com>
2021-07-15 16:21:19 -0300

    Versão Inicial do Projeto

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Repositories from './Pages/Repositories';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App title = "Hello" user="Gama Academy"/>,
   document.getElementById('root')
 );
 
